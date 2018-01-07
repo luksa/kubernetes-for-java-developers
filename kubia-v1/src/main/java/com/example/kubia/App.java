c2123d87b799d8c4249f42f3bc8e52fc015732c4
Marko Luksa <marko.luksa@gmail.com>
2018-01-07 16:13:23 +0100

    Initial commit

---
+++ kubia-v1/src/main/java/com/example/kubia/App.java
@@ -0,0 +1,39 @@
+package com.example.kubia;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Kubia server starting...");
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            System.out.println("Received request from " + t.getRemoteAddress().getAddress().getHostAddress());
+            String response = "You've hit " + InetAddress.getLocalHost().getHostName() + "\n";
+            sendResponse(t, response);
+        }
+
+        private void sendResponse(HttpExchange t, String response) throws IOException {
+            byte[] responseBytes = response.getBytes();
+            t.sendResponseHeaders(200, responseBytes.length);
+            try (OutputStream os = t.getResponseBody()) {
+                os.write(responseBytes);
+            }
+        }
+    }
+
+}
